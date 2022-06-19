d197be437ea16f7983184e66f7b78359381a6bbd
d-darwin <d@darwin.st>
2022-06-19 15:39:14 +0300

    [DVU-12] (refac) fix crlf issue

---
+++ packages/vue-ui-codegen/dist/constants/size.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+exports.SIZE = void 0;
+exports.SIZE = {
+    TINY: "tiny",
+    SMALL: "small",
+    MEDIUM: "medium",
+    LARGE: "large",
+    HUGE: "huge"
+};
