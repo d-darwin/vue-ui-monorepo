4f18e821571ecd986389fc64cac9e5722b3d5fe5
d-darwin <d@darwin.st>
2022-06-24 09:53:29 +0300

    [DVU-12] (feat) add motion design tokens

--- packages/vue-ui-style-dictionary/build.js
+++ packages/vue-ui-style-dictionary/build.js
@@ -1,17 +1,16 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 const StyleDictionary = require("style-dictionary").extend("config.json");
 const transformSizePixel = require("./transforms/size-pixel");
 const transformSpacingPixel = require("./transforms/spacing-pixel");
 const transformRadiusPixel = require("./transforms/radius-pixel");
 const transformBorderPixelStyleColor = require("./transforms/border-pixel-style");
 const transformFontString = require("./transforms/font-string");
-const transformMotionTransition = require("./transforms/motion-transition");
+const transformMotion = require("./transforms/motion");
 
 StyleDictionary.registerTransform(transformSizePixel);
 StyleDictionary.registerTransform(transformSpacingPixel);
 StyleDictionary.registerTransform(transformRadiusPixel);
 StyleDictionary.registerTransform(transformBorderPixelStyleColor);
 StyleDictionary.registerTransform(transformFontString);
-StyleDictionary.registerTransform(transformMotionTransition);
+StyleDictionary.registerTransform(transformMotion);
 
 StyleDictionary.buildAllPlatforms();
