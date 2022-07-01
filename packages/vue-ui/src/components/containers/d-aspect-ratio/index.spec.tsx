9a091ecff1a1ba05a8537df8f5a832e41d4f3c86
d-darwin <d@darwin.st>
2022-07-01 09:39:39 +0300

    [DVU-32] (test) validator test cases

--- packages/vue-ui/src/components/containers/d-aspect-ratio/index.spec.tsx
+++ packages/vue-ui/src/components/containers/d-aspect-ratio/index.spec.tsx
@@ -7,8 +7,6 @@ import {
 } from "@/utils/test-case-factories";
 
 describe("DTypography", () => {
-  const CSS = {};
-
   const wrapper = shallowMount(DAspectRatio);
 
   propHtmlCase(wrapper);
@@ -16,4 +14,38 @@ describe("DTypography", () => {
   slotDefaultCase(wrapper);
 
   baseClassCase(wrapper, "dAspectRatio");
+
+  const aspectRatioValidator = DAspectRatio.props.aspectRatio.validator;
+
+  it("Validator pass prop.aspectRatio = 0.66", () => {
+    expect(aspectRatioValidator(0.66)).toBe(true);
+  });
+
+  it("Validator passes prop.aspectRatio = 3 / 2", () => {
+    expect(aspectRatioValidator("2 / 3")).toBe(true);
+  });
+
+  it("Validator passes prop.aspectRatio = 2 : 1", () => {
+    expect(aspectRatioValidator("2 : 1")).toBe(true);
+  });
+
+  it("Validator rejects prop.aspectRatio = 0. 66", () => {
+    expect(aspectRatioValidator("0. 66")).toBe(false);
+  });
+
+  it("Validator rejects prop.aspectRatio = 3 \\ 2", () => {
+    expect(aspectRatioValidator("2 \\ 3")).toBe(false);
+  });
+
+  it("Validator rejects prop.aspectRatio = 2 : 1", () => {
+    expect(aspectRatioValidator("2 ; 1")).toBe(false);
+  });
+
+  // TODO: tag
+  it("Renders props.tag when passed", async () => {
+    const tag = "picture";
+    await wrapper.setProps({ tag });
+    expect(wrapper.element.tagName).toEqual(tag.toLocaleUpperCase());
+  });
+  // TODO: padding branch ???
 });
