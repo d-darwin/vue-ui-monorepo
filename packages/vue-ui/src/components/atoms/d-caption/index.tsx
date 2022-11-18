8ccbf591aa0cd5d83c8d1dc170117fb00d72e1d1
d-darwin <d@darwin.st>
2022-11-18 10:01:44 +0300

    [DVU-34] (maintenance) mark as deprecated

--- packages/vue-ui/src/components/atoms/d-caption/index.tsx
+++ packages/vue-ui/src/components/atoms/d-caption/index.tsx
@@ -12,7 +12,7 @@ import config from "./config";
 import styles from "./index.css?module";
 
 /**
- * A clickable component which renders as <b>button</b> element, <b>router-link</b> component or <b>a</b> element depending on props.
+ * Deprecated
  */
 export default defineComponent({
   name: config.name,
