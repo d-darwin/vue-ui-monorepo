69dd4d3b17d0a0ddbfbf1c0df6bbf82ac6f041e3
d-darwin <d@darwin.st>
2023-02-26 12:44:37 +0300

    [DVU-15] (test) scaffold test cases

--- packages/vue-ui/src/components/containers/d-grid/index.spec.ts
+++ packages/vue-ui/src/components/containers/d-grid/index.spec.ts
@@ -1,3 +1,13 @@
-// TODO: class
-// TODO: props
-// TODO: ???
+import { mount } from "@vue/test-utils";
+import DGrid from "@/components/containers/d-grid/index";
+import { baseClassCase } from "@/utils/test-case-factories";
+import config from "./config";
+
+describe("DGrid", () => {
+  const wrapper = mount(DGrid);
+
+  baseClassCase(wrapper, config.className);
+
+  // TODO: props
+  // TODO: ???
+});
