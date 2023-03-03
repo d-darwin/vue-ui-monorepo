adda741b27d0d414690a2e3bdce55a249f07b67f
d-darwin <d@darwin.st>
2023-03-03 09:59:01 +0300

    [DVU-41] (test) scaffold test cases

--- packages/vue-ui/src/components/atoms/d-backdrop/index.spec.ts
+++ packages/vue-ui/src/components/atoms/d-backdrop/index.spec.ts
@@ -1,17 +1,42 @@
 import { shallowMount } from "@vue/test-utils";
-import { baseClassCase } from "@/utils/test-case-factories";
+import {
+  baseClassCase,
+  callWhenClickCase,
+  colorSchemeClassCase,
+  tagCase,
+} from "@/utils/test-case-factories";
 import DBackdrop from "@/components/atoms/d-backdrop";
 import config from "./config";
+import { COLOR_SCHEME } from "@darwin-studio/ui-codegen/dist/constants/color-scheme";
 
 describe("DButton", () => {
   const wrapper = shallowMount(DBackdrop);
   baseClassCase(wrapper, config.className);
 
   //TODO: opacity
+  it("Should render props.opacity to the component style as '--opacity: <number>'", async () => {
+    expect(true).toBeFalsy();
+  });
+
   //TODO: zIndex
+  it("Should render props.zIndex to the component style as '--z-index: <number>'", async () => {
+    expect(true).toBeFalsy();
+  });
+
   //TODO: colorScheme
+  //  "--background-color": `var(--color-${this.colorScheme}-background)`
+  colorSchemeClassCase(
+    wrapper,
+    `.${config.className}`,
+    COLOR_SCHEME.ALTERNATIVE
+  );
+
   //TODO: tag
+  tagCase(wrapper);
+
   //TODO: when\onClick
-  //TODO: bindings
-  //TODO: required styles ???
+  it("Should emit onClick event on click", async () => {
+    expect(true).toBeFalsy();
+  });
+  callWhenClickCase(wrapper);
 });
