bcc2585bbbba8961ce4ff302587f803939ade542
d-darwin <d@darwin.st>
2023-03-08 13:53:14 +0300

    [DVU-40] (chore) add all current components to the components/index

--- packages/vue-ui/src/components/index.ts
+++ packages/vue-ui/src/components/index.ts
@@ -1,6 +1,7 @@
 import { App, Plugin } from "vue";
 
-/** atoms */
+/** atoms **/
+import DBackdrop from "./atoms/d-backdrop";
 import DButton from "./atoms/d-button";
 import DCaption from "./atoms/d-caption";
 import DCheckbox from "./atoms/d-checkbox";
@@ -12,19 +13,22 @@ import DResponsiveImage from "./atoms/d-responsive-image";
 import DSwitch from "./atoms/d-switch";
 import { DTabs, DTab, DTabpanel } from "./atoms/d-tabs";
 import DTooltip from "./atoms/d-tooltip";
-/** containers */
+/** containers **/
 import DAspectRatio from "./containers/d-aspect-ratio";
-// TODO import DGrid from "./containers/d-grid";
+import DGrid from "./containers/d-grid";
 import DTypography from "./containers/d-typography";
-/** molecules */
+/** molecules **/
 import DCheckboxGroup from "./molecules/d-checkbox-group";
 import DRadioGroup from "./molecules/d-radio-group";
 import DTable from "./molecules/d-table";
+/** organisms **/
+import DDrawer from "./organisms/d-drawer";
 
 // TODO: add all the components
 export default {
   install(Vue: App) {
     Vue.component(DButton.name, DButton);
+    Vue.component(DBackdrop.name, DBackdrop);
     Vue.component(DCaption.name, DCaption);
     Vue.component(DCheckbox.name, DCheckbox);
     Vue.component(DInput.name, DInput);
@@ -38,15 +42,17 @@ export default {
     Vue.component(DTabpanel.name, DTabpanel);
     Vue.component(DTooltip.name, DTooltip);
     Vue.component(DAspectRatio.name, DAspectRatio);
-    // TODO: Vue.component(DGrid.name, DGrid);
+    Vue.component(DGrid.name, DGrid);
     Vue.component(DTypography.name, DTypography);
     Vue.component(DCheckboxGroup.name, DCheckboxGroup);
     Vue.component(DRadioGroup.name, DRadioGroup);
     Vue.component(DTable.name, DTable);
+    Vue.component(DDrawer.name, DDrawer);
   },
 } as Plugin;
 
 export {
+  DBackdrop,
   DButton,
   DCaption,
   DCheckbox,
@@ -61,9 +67,10 @@ export {
   DTabpanel,
   DTooltip,
   DAspectRatio,
-  // TODO: DGrid
+  DGrid,
   DTypography,
   DCheckboxGroup,
   DRadioGroup,
   DTable,
+  DDrawer,
 };
